78dbc896f3e81227bc9611144e78dd9e6de03aa3
steven-adrnn <131145320+steven-adrnn@users.noreply.github.com>
2025-03-08 15:25:12 +0700

    Deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,12 @@
 
 # Jalankan migrasi & bersihkan cache
 php artisan migrate --force
+php artisan optimize
 php artisan config:clear
 php artisan cache:clear
 php artisan route:clear
 php artisan view:clear
+php artisan storage:link
 
 # Jalankan Laravel dengan PORT dari Railway
 echo "Starting Laravel on port $PORT..."
