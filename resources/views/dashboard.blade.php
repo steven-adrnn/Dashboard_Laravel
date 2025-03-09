7fa92960b1f25f33eb06682dfa24553bcb45e38e
steven-adrnn <131145320+steven-adrnn@users.noreply.github.com>
2025-03-10 04:31:06 +0700

    Edit Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <h3 class="text-lg font-medium">Daftar Buku</h3>
                     @if(auth()->user()->hasRole('admin') || auth()->user()->hasRole('editor')) <!-- Check if the user has the admin or editor role -->
-                        <a href="{{ route('dashboard.create') }}" class="btn btn-primary mt-4">Tambah Buku</a>
+                        <a href="{{ route('dashboard.create') }}" class="btn btn-primary mt-4" title="Tambah Buku">Tambah Buku</a>
                     @endif
                     <table class="min-w-full mt-4">
                         <thead>
@@ -31,7 +31,7 @@
                                     <td class="border px-4 py-2">{{ $book->deskripsi }}</td>
                                     @if(auth()->user()->hasRole('admin') || auth()->user()->hasRole('editor')) <!-- Check if the user has the admin or editor role -->
                                         <td class="border px-4 py-2">
-                                                <a href="{{ route('dashboard.edit', $book->id) }}" class="btn btn-secondary ml-2">Edit Buku</a>
+                                                <a href="{{ route('dashboard.edit', $book->id) }}" class="btn btn-secondary ml-2" title="Edit Buku">Edit Buku</a>
                                         </td>
                                     @endif
                                     @if(auth()->user()->hasRole('admin')) <!-- Check if the user has the admin role -->
@@ -39,7 +39,7 @@
                                             <form action="{{ route('dashboard.destroy', $book->id) }}" method="POST" onsubmit="return confirm('Apakah Anda yakin ingin menghapus buku ini?');">
                                                 @csrf
                                                 @method('DELETE')
-                                                <button type="submit" class="btn btn-danger">Hapus Buku</button>
+                                                <button type="submit" class="btn btn-danger" title="Hapus Buku">Hapus Buku</button>
                                             </form>
                                         </td>
                                     @endif
