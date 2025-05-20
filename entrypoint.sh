1245e30a3aa58e38425f1cbf402a4a612b302ece
steven-adrnn <131145320+steven-adrnn@users.noreply.github.com>
2025-05-21 06:30:30 +0700

    fix deploy problem

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,11 @@ php artisan route:clear
 php artisan view:clear
 php artisan storage:link
 
+# Jalankan seeder hanya jika environment bukan production
+if [ "$APP_ENV" != "production" ]; then
+  php artisan db:seed --force
+fi
+
 # Jalankan Laravel dengan PORT dari Railway
 echo "Starting Laravel on port $PORT..."
 php artisan serve --host=0.0.0.0 --port=$PORT
